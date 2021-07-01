ce1391896865bcba747724f7b7c0ccdd75b95fce
Basil Leju <bas.mak3r@gmail.com>
2021-07-01 17:46:00 +0530

    ui

--- frontend/one_way_client.py
+++ frontend/one_way_client.py
@@ -2,49 +2,52 @@ from _thread import *
 import sys
 import threading
 import socket
-from termcolor import colored   #for highlighting
+from termcolor import colored  # for highlighting
 
 if len(sys.argv) < 2:
-	#print(colored("[INFO] ",'green'),end="")
-	#print(colored("Usage : python3 one_way_client.py <filename>",'white'))
-	sys.exit()
+    #print(colored("[INFO] ",'green'),end="")
+    #print(colored("Usage : python3 one_way_client.py <filename>",'white'))
+    sys.exit()
 else:
-	filename = sys.argv[1]
+    filename = sys.argv[1]
+    sys.stdout.flush()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 try:
-	sock.connect(('127.0.0.1',8888))
-	#print(colored("[INFO] ",'green'),end="")
-	#print(colored("connected to File Server\n",'white'))
+    sock.connect(('127.0.0.1', 8888))
+    #print(colored("[INFO] ",'green'),end="")
+    #print(colored("connected to File Server\n",'white'))
 except Exception as e:
-	#print(colored("[INFO] ",'green'),end="")
-	print("Unable to connect to File Server\n")
-	sys.exit()
+    #print(colored("[INFO] ",'green'),end="")
+    print("Unable to connect to File Server\n")
+    sys.exit()
+
 
 def client(filename):
-	while True:
-		try:
-			message=filename
-			if message == 'exit':
-				break
-			elif message == "":
-				break
-			else:
-				msg = message
-				sock.send(msg.encode())
+    while True:
+        try:
+            message = filename
+            if message == 'exit':
+                break
+            elif message == "":
+                break
+            else:
+                msg = message
+                sock.send(msg.encode())
 
-				message = sock.recv(2048)
-				if message.decode() == "exit":
-					print("Lost connection with Proxy Server")
-				else:
-					print(message.decode())
+                message = sock.recv(2048)
+                if message.decode() == "exit":
+                    print("Lost connection with Proxy Server")
+                else:
+                    print(message.decode())
 
-				sock.close()
-				break
+                sock.close()
+                break
 
-		except Exception as e:
-			print("Unable to connect")
-			break
+        except Exception as e:
+            print("Unable to connect")
+            break
+
 
 client(filename)
--- frontend/renderer.js
+++ frontend/renderer.js
@@ -1,18 +1,24 @@
-const {PythonShell} =require('python-shell');
+const { PythonShell } = require('python-shell');
 
 document.getElementById("search").addEventListener("click", function() {
-          
-    var filename = document.getElementById("filein").value;
-    let options = {
-      mode: 'text',
-      pythonPath: 'python3',
-      pythonOptions: ['-u'],
-      scriptPath: './',
-      args: [filename]
-    };
 
-    PythonShell.run('one_way_client.py', options, function(err,results){
-      if (err) throw err;
-      alert(results)
+    var filename = document.getElementById("filein").value;
+    // alert(filename)
+    let options = {
+        mode: 'text',
+        pythonPath: 'python',
+        pythonOptions: ['-u'],
+        scriptPath: './',
+        args: [filename]
+    };
+    // url = 'C://Users/91974/Desktop/Projects/ProxyServer/frontend/child.html?name=' + encodeURIComponent(filename);
+
+    // document.location.href = filename;
+    PythonShell.run('one_way_client.py', options, function(err, results) {
+        if (err) throw err;
+        // alert(results)
+        url = 'C://Users/91974/Desktop/Projects/ProxyServer/frontend/child.html?name=' + encodeURIComponent(results);
+        document.location.href = url;
     });
+    // alert(filename)
 });
