03c533fcdda4cdece8d3329246df258da6d2d670
Basil Leju <bas.mak3r@gmail.com>
2021-07-01 17:46:07 +0530

    ui

--- frontend/renderer.js
+++ frontend/renderer.js
@@ -1,5 +1,21 @@
 const { PythonShell } = require('python-shell');
 
+g = "hai"
+window.onload = function() {
+    var url = document.location.href,
+        params = url.split('?')[1].split('&'),
+        data = {},
+        tmp;
+    alert(params)
+    for (var i = 0, l = params.length; i < l; i++) {
+        tmp = params[i].split('=');
+        data[tmp[0]] = tmp[1];
+    }
+
+    document.getElementById('here').innerHTML = data.search;
+}
+
+
 document.getElementById("search").addEventListener("click", function() {
 
     var filename = document.getElementById("filein").value;
@@ -11,14 +27,11 @@ document.getElementById("search").addEventListener("click", function() {
         scriptPath: './',
         args: [filename]
     };
-    // url = 'C://Users/91974/Desktop/Projects/ProxyServer/frontend/child.html?name=' + encodeURIComponent(filename);
+    // url = './child.html?name=' + encodeURIComponent(results);
 
-    // document.location.href = filename;
     PythonShell.run('one_way_client.py', options, function(err, results) {
         if (err) throw err;
-        // alert(results)
-        url = 'C://Users/91974/Desktop/Projects/ProxyServer/frontend/child.html?name=' + encodeURIComponent(results);
-        document.location.href = url;
+        window.location.href = "child.html?data=" + results;
+
     });
-    // alert(filename)
 });
